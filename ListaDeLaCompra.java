87f09cd147a1725a357ee8525810a8f7843a66f2
Alejandro Soler <just.asc99@gmail.com>
2024-11-28 20:16:39 +0100

    Version 1

---
+++ ListaDeLaCompra.java
@@ -0,0 +1,10 @@
+
+public class ListaDeLaCompra {
+
+    public static void main(String[] args) {
+        System.out.println("Patatas");
+        System.out.println("Manazanas");
+        System.out.println("Palomitas");
+    }
+    
+}
